c732910cf3459b9d12598586f7336c0e144158b8
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-04-29 20:14:49 +0530

    implementation done for fetching Lat and Lon

--- app/src/main/java/com/amitupadhyay/getlocationfast/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/getlocationfast/MainActivity.java
@@ -1,50 +1,123 @@
 package com.amitupadhyay.getlocationfast;
 
+import android.app.ProgressDialog;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.yayandroid.locationmanager.configuration.Configurations;
 import com.yayandroid.locationmanager.configuration.LocationConfiguration;
+import com.yayandroid.locationmanager.constants.ProcessType;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends LocationBaseActivity implements SamplePresenter.SampleView {
 
+    @BindView(R.id.getLocationBtn)
+    Button getLocationBtn;
+
+    @BindView(R.id.showLocationTV)
+    TextView showLocationTV;
+
+    @OnClick(R.id.getLocationBtn)
+    void onClickGetLocationBtn()
+    {
+        getLocation();
+    }
+
+    private ProgressDialog progressDialog;
+    private SamplePresenter samplePresenter;
+
     @Override
     public LocationConfiguration getLocationConfiguration() {
-        return null;
+        return Configurations.defaultConfiguration("Gimme the permission!", "Would you mind to turn GPS on?");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        samplePresenter = new SamplePresenter(this);
+
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (getLocationManager().isWaitingForLocation()
+                && !getLocationManager().isAnyDialogShowing()) {
+            displayProgress();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        dismissProgress();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        samplePresenter.destroy();
+    }
+
+    private void displayProgress() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.getWindow().addFlags(Window.FEATURE_NO_TITLE);
+            progressDialog.setMessage("Getting location...");
+        }
+
+        if (!progressDialog.isShowing()) {
+            progressDialog.show();
+        }
+    }
+
+
+    @Override
     public String getText() {
-        return null;
+        return showLocationTV.getText().toString();
     }
 
     @Override
     public void setText(String text) {
-
+        showLocationTV.setText(text);
     }
 
     @Override
     public void updateProgress(String text) {
-
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.setMessage(text);
+        }
     }
 
     @Override
     public void dismissProgress() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
 
+    @Override
+    public void onProcessTypeChanged(@ProcessType int processType) {
+        samplePresenter.onProcessTypeChanged(processType);
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
+        samplePresenter.onLocationChanged(location);
     }
 
     @Override
     public void onLocationFailed(int type) {
-
+        samplePresenter.onLocationFailed(type);
     }
 }
